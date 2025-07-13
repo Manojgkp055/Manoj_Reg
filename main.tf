6cde335f78990faf28def34cc7a8cff7a66b0230
Manojgkp055 <mk375509@gmail.com>
2025-07-13 15:08:18 +0530

    new code (#1)
    Ready for production

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "manoj" {
   name     = "mkk21"
   location = "eastus"
 }
+resource "azurerm_resource_group" "manoj11" {
+  name     = "Manoj11"
+  location = "eastus"
+}
