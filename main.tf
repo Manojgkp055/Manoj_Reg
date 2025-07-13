4eb0f1ab7948728fbd626aacb0124865add847fe
Manoj Kumar2 <manoj.kumar2@dishtv.in>
2025-07-13 15:41:23 +0530

    B3 code

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "manoj11" {
   name     = "Manoj11"
   location = "eastus"
 }
+resource "azurerm_resource_group" "manoj33" {
+  name     = "Manoj33"
+  location = "eastus"
+}
